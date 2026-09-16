848f066d21286447e225fe3f5f280820de91167a
Nihala-ms <nihalabintsalih@gmail.com>
2026-09-16 11:01:00 +0530

    first commit

--- tripmate-app/src/services/api.js
+++ tripmate-app/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://tripmate-server-9kffqz2xj-nihala-ms-projects.vercel.app/api";
 
 export const sendTripDetails = async (tripData) => {
   try {
